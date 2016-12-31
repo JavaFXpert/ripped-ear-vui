6e8667bd3a775381abdb77fa474421cf1bc39ce3
JavaFXpert <james@j1w.xyz>
2016-12-31 05:40:18 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+// Copyright 2017 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+let Assistant = require('actions-on-google').ApiAiAssistant;
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+app.use(bodyParser.json({type: 'application/json'}));
+
+const NAME_ACTION = 'random_interval';
+const TYPE_ARGUMENT = 'type';
+
+// [START RippedEar]
+app.post('/', function (req, res) {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  // Generate a random interval
+  function randomInterval (assistant) {
+    //let type = assistant.getArgument(TYPE_ARGUMENT);
+    assistant.tell('<speak>Here is the interval <audio src="https://storage.googleapis.com/musicnotes/40.wav"/><audio src="https://storage.googleapis.com/musicnotes/44.wav"/></speak>');
+  }
+
+  let actionMap = new Map();
+  actionMap.set(NAME_ACTION, randomInterval);
+
+  assistant.handleRequest(actionMap);
+});
+// [END RippedEar]
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+  // [END server]
+}
+
+module.exports = app;
